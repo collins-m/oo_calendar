7c0e5b8dd11a73409f0652df79ff395603d73444
Michael Collins <collins.m@pm.me>
2018-11-20 14:33:16 +0000

    Add files via upload

---
+++ day.py
@@ -0,0 +1,25 @@
+from appointment import Appointment
+
+class Day:
+
+	def __init__(self, name):
+		""" Initialized with a name, and a list of appointments.
+		"""
+		self.id = name
+		self.appointments = []
+
+	def add_appointment(self, name, start, end):
+		""" Using the Appointment class, we append an Appointment object to the list.
+		"""
+		name = Appointment(name, start, end)
+		self.appointments.append(name)
+
+	def remove_appointment(self, name):
+		""" We remove the selected Appointment from the object.
+		"""
+		pass
+	
+	def __str__(self):
+		""" Prints the Day, and its list of Appointments.
+		"""
+		pass
