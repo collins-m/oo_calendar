c3c5128d6ab68f2181c6e1333b778dafd7a3296b
Michael Collins <collins.m@pm.me>
2018-11-20 14:34:42 +0000

    Added Directory

---
+++ src/week.py
@@ -0,0 +1,11 @@
+class Week:
+
+	def __init__(self):
+		""" Initialized with a list of Day objects.
+		"""
+		self.week =  []
+
+	def __str__(self):
+		""" Prints each of the Days in turn.
+		"""
+		pass
